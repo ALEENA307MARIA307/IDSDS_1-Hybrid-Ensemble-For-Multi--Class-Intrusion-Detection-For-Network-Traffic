837f2b61af66edc349b0f48d8291669fcef811b9
ALEENA MARIA ANTONY <aleenamariaantony307@gmail.com>
2025-11-08 14:22:30 +0000

    Update ensemble.py

--- deployment/ensemble.py
+++ deployment/ensemble.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import pandas as pd
 
@@ -9,13 +8,22 @@ class EnsemblePredictor:
         self.voting = voting
         self.b_models = b_models
 
-    def predict(self, X):
-        base_probs = np.hstack([m.predict_proba(X) for m in self.base_models])
+    def predict(self, X, X_m):
+      
+        # Base models for meta model predict on X_m
+        base_probs = np.hstack([m.predict_proba(X_m) for m in self.base_models])
+        
+        # Meta model predicts based on base_probs
         meta_prob = self.meta_model.predict_proba(base_probs)[:, 1]
 
         if self.voting == 'soft':
+            # Blending models predict on X
             b_probs = np.hstack([m.predict_proba(X) for m in self.b_models])
+            
+            # Combine meta model and blending model outputs
             all_probs = np.hstack([meta_prob.reshape(-1, 1), b_probs])
+            
+            # Average probabilities
             final_probs = np.mean(all_probs, axis=1)
             return (final_probs >= 0.5).astype(int)
         else:
