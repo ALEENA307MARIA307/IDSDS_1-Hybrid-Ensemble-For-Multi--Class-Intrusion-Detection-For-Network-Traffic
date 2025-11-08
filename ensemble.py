cdf4cb8b2e1666388f6634370361b8471f563652
ALEENA MARIA ANTONY <aleenamariaantony307@gmail.com>
2025-11-08 02:42:12 +0000

    initial commit

---
+++ ensemble.py
@@ -0,0 +1,22 @@
+
+import numpy as np
+import pandas as pd
+
+class EnsemblePredictor:
+    def __init__(self, base_models, meta_model, b_models, voting='soft'):
+        self.base_models = base_models
+        self.meta_model = meta_model
+        self.voting = voting
+        self.b_models = b_models
+
+    def predict(self, X):
+        base_probs = np.hstack([m.predict_proba(X) for m in self.base_models])
+        meta_prob = self.meta_model.predict_proba(base_probs)[:, 1]
+
+        if self.voting == 'soft':
+            b_probs = np.hstack([m.predict_proba(X) for m in self.b_models])
+            all_probs = np.hstack([meta_prob.reshape(-1, 1), b_probs])
+            final_probs = np.mean(all_probs, axis=1)
+            return (final_probs >= 0.5).astype(int)
+        else:
+            return (meta_prob >= 0.5).astype(int)
